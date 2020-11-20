9f146a0642f638994c48aaa7e3001b0f8aaf2fc9
Jun Luo <junluo2@illinois.edu>
2020-11-19 19:01:04 -0500

    Build correct fullyQualifiedName for nested test
    For a nested test the correct fully qualified name should be:
    com.package.ClassA$NestedClassB#TestC     pay attention to $
    
    In this case, the Class.getName returns: com.package.ClassA$NestedClassB
    While Class.getCanonicalName return: com.package.ClassA.NestedClassB (wrong)
    
    Also, add a regex to parse the nested test uniqueId.

--- testrunner-running/src/main/scala/edu/illinois/cs/testrunner/execution/Utils.java
+++ testrunner-running/src/main/scala/edu/illinois/cs/testrunner/execution/Utils.java
@@ -4,6 +4,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Utils {
+    private static final String junit5NestedRegex = "\\[class:([\\w.]+).*\\[nested-class:([\\w.]+).*\\[method:(\\w+)\\(";
+    private static final Pattern junit5NestedPattern = Pattern.compile(junit5NestedRegex);
     private static final String junit5Regex = "\\[class:([\\w.]+).*\\[method:(\\w+)\\(";
     private static final Pattern junit5Pattern = Pattern.compile(junit5Regex);
     private static final String junit4Regex = "\\[test:(\\w+)\\(([\\w.]+)\\)";
@@ -11,6 +13,10 @@ public class Utils {
 
     /**
      * Turn the uniqueId from identifier into fully qualified method name.
+     *
+     * For JUnit 5 nested test:
+     * uniqueId: [engine:junit-jupiter]/[class:com.luojl.demo.InheritedTest]/[nested-class:NestedTest]/[method:NestedTestB()]
+     * fully qualified name: com.luojl.demo.InheritedTest$NestedTest#NestedTestB
      *
      * For JUnit 5:
      * uniqueId: [engine:junit-jupiter]/[class:com.luojl.demo.JUnit5DemoTest]/[method:TestC()]
@@ -21,7 +27,12 @@ public class Utils {
      * full qualified name: com.luojl.demo.JUnit4DemoTest#TestA4
      */
     public static String toFullyQualifiedName(String identifierUniqueId) {
-        Matcher matcher = junit5Pattern.matcher(identifierUniqueId);
+        Matcher matcher = junit5NestedPattern.matcher(identifierUniqueId);
+        if (matcher.find()) {
+            // found JUnit 5 nested test pattern
+            return matcher.group(1) + "$" + matcher.group(2) + "#" + matcher.group(3);
+        }
+        matcher = junit5Pattern.matcher(identifierUniqueId);
         if (matcher.find()) {
             // found JUnit 5 pattern
             return matcher.group(1) + "#" + matcher.group(2);
